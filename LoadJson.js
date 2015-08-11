5eadaa9a3371c2ad9c3da5d5cf598600c297e4d0
sandsmi <sandra.smietana@onet.eu>
2015-08-11 12:51:19 +0200

    Better update

--- LoadJson.js
+++ LoadJson.js
@@ -3,15 +3,13 @@ var root = 'http://jsonplaceholder.typicode.com';
     url: root + '/posts/1',
     method: 'GET'
   }).then(function(data) {
-    return data;
-  });
-
-    //var str = JSON.stringify(data, null, '\t');
+    var str = JSON.stringify(data, null, '\t');
     //obj = JSON.parse(str);
-    var obj = (function(data){});
+    var obj = data;
     $.each(obj, function(i, elem) {
       $('#userId').text(obj.userId);
       $('#id').text(obj.id);
       $('#title').text(obj.title);
       $('#post').text(obj.body);
     });
+  });
