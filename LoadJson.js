b6c927d6e17078a6534eaba7b8c781d18a00527a
sandsmi <sandra.smietana@onet.eu>
2015-08-11 15:02:25 +0200

    "Version that actually works"

--- LoadJson.js
+++ LoadJson.js
@@ -1,15 +1,9 @@
-var root = 'http://jsonplaceholder.typicode.com';
-  $.ajax({
-    url: root + '/posts/1',
-    method: 'GET'
-  }).then(function(data) {
-    var str = JSON.stringify(data, null, '\t');
-    //obj = JSON.parse(str);
-    var obj = data;
-    $.each(obj, function(i, elem) {
-      $('#userId').text(obj.userId);
-      $('#id').text(obj.id);
-      $('#title').text(obj.title);
-      $('#post').text(obj.body);
-    });
+$.getJSON('http://jsonplaceholder.typicode.com/posts',
+  function(data) {
+      $.each(data, function(i) {
+        $('<div id="userId">'+data[i].userId+'</div>').appendTo('#container');
+        $('<div id="id">'+data[i].id+'</div>').appendTo('#container');
+        $('<div id="title">'+data[i].title+'</div>').appendTo('#container');
+        $('<div id="post">'+data[i].body+'</div>').appendTo('#container');
+      });
   });
