1c49f76886c7ef89d573f7404220eec4c6ec2057
sandsmi <sandra.smietana@onet.eu>
2015-08-12 09:26:55 +0200

    "Working infinite scroll"

--- LoadJson.js
+++ LoadJson.js
@@ -1,3 +1,6 @@
+var postLimit = 15;
+
+// Loads placeholder JSON data from site
 $.getJSON('http://jsonplaceholder.typicode.com/posts',
   function(data) {
       $.each(data, function(i) {
@@ -8,4 +11,23 @@ $.getJSON('http://jsonplaceholder.typicode.com/posts',
         +data[i].title+'</div><div class="text">'
         +data[i].body+'</div> </div>');
       });
+      // Hides posts after the set limit
+      $('#container > .post:gt('+(postLimit)+')').hide();
   });
+
+// Detects scrollbar position
+$(window).scroll(function() {
+      if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        loadMore();
+      }
+    });
+
+// Loads five more posts if the scrollbar reaches the bottom of the page
+function loadMore()
+  {
+    postLimit += 5;
+    $('#container > .post:lt('+(postLimit)+')').show();
+    if ($('#container > .post').length <= postLimit) {
+        $(this).hide();
+    }
+  };
