6f7d0dcaad1a123dd2fc9adc99c1d53c9baa7aa6
sandsmi <sandra.smietana@onet.eu>
2015-08-13 09:29:19 +0200

    "Added other versions in comments"

--- LoadJson.js
+++ LoadJson.js
@@ -1,6 +1,7 @@
 var page = 1;
 
-var loadData = function(page) {
+// loads placeholder data from site
+var LoadData = function(page) {
     var maxPostLimit = page * 20;
     var minPostLimit = maxPostLimit - 19;
     $.ajax({
@@ -20,15 +21,18 @@ var loadData = function(page) {
     });
 };
 
-loadData(page);
+// Loads data the first time
+LoadData(page);
 
-// Detects scrollbar position
+// Detects scrollbar position and loads data at the bottom of the page
 $(window).scroll(function() {
       if ($(window).scrollTop() == $(document).height() - $(window).height()) {
           page++;
-          loadData(page);
+          LoadData(page);
       }
 });
+
+// PREVIOUS VERSION
 
   /*
   var postLimit = 15;
@@ -63,4 +67,34 @@ $(window).scroll(function() {
           $(this).hide();
       }
     };
+*/
+
+// DIFFERENT APPROACH (UNFINISHED!!)
+
+/*
+var page = 1;
+
+var LoadData = function(page) {
+  var maxPostLimit = page * 20;
+  var minPostLimit = maxPostLimit - 1;
+  $.ajax({
+      method: "GET",
+      dataType: 'json',
+      url: 'http://jsonplaceholder.typicode.com/posts/',
+      success: (function (data) {
+          for (i = minPostLimit; i <= maxPostLimit; i++) {
+
+          }
+      })
+  });
+}
+
+var DisplayData = function() {
+    $('#container').append
+    ('<div class="post"><div class="userId">'
+    +data[i].userId+'</div><div class="id">'
+    +data[i].id+'</div><div class="title">'
+    +data[i].title+'</div><div class="text">'
+    +data[i].body+'</div> </div>');
+}
 */
