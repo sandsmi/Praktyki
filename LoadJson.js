0b7afc2f83390dfe780d5cccaa651743cc247f98
sandsmi <sandra.smietana@onet.eu>
2015-08-13 11:49:45 +0200

    "Added styles"

--- LoadJson.js
+++ LoadJson.js
@@ -4,6 +4,9 @@ var page = 1;
 var LoadData = function(page) {
     var maxPostLimit = page * 20;
     var minPostLimit = maxPostLimit - 19;
+    if(page = 1) {
+        minPostLimit = 0;
+    }
     $.ajax({
         method: "GET",
         dataType: 'json',
@@ -13,7 +16,7 @@ var LoadData = function(page) {
                 $('#container').append
                 ('<div class="post"><div class="userId">'
                 +data[i].userId+'</div><div class="id">'
-                +data[i].id+'</div><div class="title">'
+                + "#" +data[i].id+'</div><div class="title">'
                 +data[i].title+'</div><div class="text">'
                 +data[i].body+'</div> </div>');
             }
@@ -31,43 +34,6 @@ $(window).scroll(function() {
           LoadData(page);
       }
 });
-
-// PREVIOUS VERSION
-
-  /*
-  var postLimit = 15;
-
-  // Loads placeholder JSON data from site
-  $.getJSON('http://jsonplaceholder.typicode.com/posts',
-    function(data) {
-        $.each(data, function(i) {
-          $('#container').append
-          ('<div class="post"><div class="userId">'
-          +data[i].userId+'</div><div class="id">'
-          +data[i].id+'</div><div class="title">'
-          +data[i].title+'</div><div class="text">'
-          +data[i].body+'</div> </div>');
-        });
-        // Hides posts after the set limit
-        $('#container > .post:gt('+(postLimit)+')').hide();
-    });
-
-  // Detects scrollbar position
-  $(window).scroll(function() {
-        if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-          loadMore();
-        }
-      });
-
-  // Loads five more posts if the scrollbar reaches the bottom of the page
-  function loadMore() {
-      postLimit += 5;
-      $('#container > .post:lt('+(postLimit)+')').show();
-      if ($('#container > .post').length <= postLimit) {
-          $(this).hide();
-      }
-    };
-*/
 
 // DIFFERENT APPROACH (UNFINISHED!!)
 
