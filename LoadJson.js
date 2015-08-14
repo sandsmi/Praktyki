879486f8a67a00b194420e023745abb24a8c7e95
sandsmi <sandra.smietana@onet.eu>
2015-08-14 15:10:39 +0200

    "Added classes"

--- LoadJson.js
+++ LoadJson.js
@@ -1,36 +1,69 @@
 var page = 1;
+var posts = [];
 
 // loads placeholder data from site
-var LoadData = function(page) {
-    var maxPostLimit = page * 20;
-    var minPostLimit = maxPostLimit - 19;
-    if(page == 1) {
-        minPostLimit = 0;
-    }
-    $.ajax({
-        method: "GET",
-        dataType: 'json',
-        url: 'http://jsonplaceholder.typicode.com/posts/',
-        success: (function (data) {
-            for (i = minPostLimit; i <= maxPostLimit; i++) {
-                $('<div class="post"><div class="icon"></div><div class="userId">'
-                +data[i].userId+'</div><div class="id">'
-                + "#" +data[i].id+'</div><div class="title">'
-                +data[i].title+'</div><div class="text">'
-                +data[i].body+'</div> </div>').hide()
-                .appendTo('#container').fadeIn("slow");
+
+var DataService = function() {
+    console.log("Created LoadData!");
+
+    var handleData = function(page, data) {
+        var maxPostLimit = page * 20;
+        var minPostLimit = maxPostLimit - 19;
+        if(page == 1) {
+            minPostLimit = 0;
+        }
+        for (i = minPostLimit; i <= maxPostLimit; i++) {
+            posts.push(data[i]);
+        }
+        console.log("It works!");
+    };
+
+    this.getJsonFromSite = function(page, callback) {
+        console.log("Loaded data from site")
+        $.ajax({
+            method: "GET",
+            dataType: 'json',
+            url: 'http://jsonplaceholder.typicode.com/posts/',
+            success: function(data) {
+              handleData(page, data);
+              if(callback)
+                callback(data);
             }
         })
-    });
+    };
+
 };
 
+var DisplayData = function() {
+    console.log("Created DisplayData!");
+
+    this.appendData = function() {
+        var minPostLimit = (page * 20) - 19;
+        if(page == 1) {
+            minPostLimit = 0;
+        }
+        for (i = minPostLimit; i < posts.length; i++) {
+            $('<div class="post"><div class="icon"></div><div class="userId">'
+            +posts[i].userId+'</div><div class="id">'
+            + "#" +posts[i].id+'</div><div class="title">'
+            +posts[i].title+'</div><div class="text">'
+            +posts[i].body+'</div> </div>').hide()
+            .appendTo('#container').fadeIn("slow");
+        }
+        console.log("Append");
+    };
+};
+
+var p = new DataService();
+var d = new DisplayData();
+
 // Loads data the first time
-LoadData(page);
+p.getJsonFromSite(page, d.appendData);
 
 // Detects scrollbar position and loads data at the bottom of the page
 $(window).scroll(function() {
       if ($(window).scrollTop() == $(document).height() - $(window).height()) {
           page++;
-          LoadData(page);
+          p.getJsonFromSite(page, d.appendData);
       }
 });
